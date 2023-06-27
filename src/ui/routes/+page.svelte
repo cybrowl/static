601bd21ad0975c78b1927534f2825d1053264562
cyberowl <cybrowl@protonmail.com>
2023-06-27 03:42:44 -0700

    fix: video player and mobile ex

--- src/ui/routes/+page.svelte
+++ src/ui/routes/+page.svelte
@@ -102,12 +102,12 @@
 
 <style lang="postcss">
 	.grid_layout {
-		@apply grid grid-cols-12 gap-y-2 relative mx-12 2xl:mx-60;
+		@apply grid grid-cols-1 md:grid-cols-12 gap-4 md:gap-y-2 relative mx-4 md:mx-12 2xl:mx-60;
 	}
 	.navigation_main_layout {
 		@apply row-start-1 row-end-auto col-start-1 col-end-13 sticky top-0 z-30 bg-backdrop;
 	}
 	.content_layout {
-		@apply row-start-4 row-end-auto grid lg:grid-cols-3 md:grid-cols-2 col-start-1 col-end-13 gap-x-6 gap-y-12 mb-16;
+		@apply row-start-2 md:row-start-4 row-end-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-x-6 md:gap-y-12 col-start-1 col-end-13 mb-16;
 	}
 </style>
